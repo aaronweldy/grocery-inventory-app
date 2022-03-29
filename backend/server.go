7a5a5bb4504aa0ac6728f7ceb2243c05d89000af
sabtt <sabtt@umich.edu>
2022-03-29 19:44:43 -0400

    tempeh is not fried chicken

--- backend/server.go
+++ backend/server.go
@@ -15,42 +15,42 @@ type Request struct {
 }
 
 type Token struct {
-	expires_in int
-	access_token string
+	Expires_in int
+	Access_token string
+	Token_type string
 }
 
-func ProductHandler(term string, locationId string) {
+func ProductHandler(term string, locationId string, token string) {
   url := fmt.Sprintf("https://api.kroger.com/v1/products?filter.term=%s&filter.locationId=%s", term, locationId)
 
   req, _ := http.NewRequest("GET", url, nil)
 
   req.Header.Add("Accept", "application/json")
-  req.Header.Add("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJlZWNzNDk3cHQyLWVmZDQ0YzllNTJkZjcwYTA2YjcwYTYxOTE0MmE0YjU2ODgyNDgzOTM0Mzk0ODc0MjgzMyIsImV4cCI6MTY0ODQ4MDYyOCwiaWF0IjoxNjQ4NDc4ODIzLCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjU0Y2I0MTc5LWExYTQtNWE2MC05ODUxLTI2YTRiNzczZTVlZSIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjQ4NDc4ODI4MDYzNjM4NTc3LCJhenAiOiJlZWNzNDk3cHQyLWVmZDQ0YzllNTJkZjcwYTA2YjcwYTYxOTE0MmE0YjU2ODgyNDgzOTM0Mzk0ODc0MjgzMyJ9.aAPRlMhWsvS7uc-_Ez56D9RheeobXCwqJKoR6olxy5GGmEInL8IvJcB15AZ2sHELzdPf51DAzXgW9CTuoSsTh0IJ7OtuCRWgkLpzHwpbhkRjlLT0V9hvszj4NhgwyA5y7J6at7FxLGfuZ1E_1c024vcVROB2pRzYXeeAdsNfLSQbqKnXd6uwToQ5qk_JSmJdLiTxCgGxY-5YIT7z-1VCiARqyjm7aY6OhT8jEbA5YjHCOLbepybH6GOxvUZlgn3uN8Uo8Yp-Vd_I2GKkgkunK0JxML7TEVDoXVFtNHaprJ9uARLUAF2B7LgnhrVRX3Y9d1dAoC8pi3L8JWggRSyOIg")
+  req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 
   res, _ := http.DefaultClient.Do(req)
 
   defer res.Body.Close()
   body, _ := ioutil.ReadAll(res.Body)
 
-  fmt.Println(res)
-  fmt.Println(string(body))
+  // fmt.Println(res)
+  // fmt.Println(string(body))
 }
 
-func LocationHandler(zip string) {
-	url := fmt.Sprintf("https://api.kroger.com/v1/locations?filter.zipCode.near=%s", zip)
-
+func LocationHandler(zip string, token string) {
+	url := fmt.Sprintf("https://api.kroger.com/v1/locations?filter.zipCode.near=%s&filter.limit=3", zip)
   req, _ := http.NewRequest("GET", url, nil)
 
   req.Header.Add("Accept", "application/json")
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJlZWNzNDk3cHQyLWVmZDQ0YzllNTJkZjcwYTA2YjcwYTYxOTE0MmE0YjU2ODgyNDgzOTM0Mzk0ODc0MjgzMyIsImV4cCI6MTY0ODQ4MDYyOCwiaWF0IjoxNjQ4NDc4ODIzLCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjU0Y2I0MTc5LWExYTQtNWE2MC05ODUxLTI2YTRiNzczZTVlZSIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjQ4NDc4ODI4MDYzNjM4NTc3LCJhenAiOiJlZWNzNDk3cHQyLWVmZDQ0YzllNTJkZjcwYTA2YjcwYTYxOTE0MmE0YjU2ODgyNDgzOTM0Mzk0ODc0MjgzMyJ9.aAPRlMhWsvS7uc-_Ez56D9RheeobXCwqJKoR6olxy5GGmEInL8IvJcB15AZ2sHELzdPf51DAzXgW9CTuoSsTh0IJ7OtuCRWgkLpzHwpbhkRjlLT0V9hvszj4NhgwyA5y7J6at7FxLGfuZ1E_1c024vcVROB2pRzYXeeAdsNfLSQbqKnXd6uwToQ5qk_JSmJdLiTxCgGxY-5YIT7z-1VCiARqyjm7aY6OhT8jEbA5YjHCOLbepybH6GOxvUZlgn3uN8Uo8Yp-Vd_I2GKkgkunK0JxML7TEVDoXVFtNHaprJ9uARLUAF2B7LgnhrVRX3Y9d1dAoC8pi3L8JWggRSyOIg")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 
   res, _ := http.DefaultClient.Do(req)
 
   defer res.Body.Close()
-  body, _ := ioutil.ReadAll(res.Body)
-
-  fmt.Println(res)
-  fmt.Println(string(body))
+  // _, _ := ioutil.ReadAll(res.Body)
+	//
+  // fmt.Println(res)
+  // fmt.Println(string(body))
 }
 
 func basicGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -78,13 +78,19 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 			return
 	}
 
-	// body = r.Body
-	req_body := Request{
-		"48104",
-		"tempeh tofu",
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(r.Body)
+	// var user_input Request
+	user_input := Request{
+		Zip: "48104",
+		Products: "tempeh",
 	}
-	zip := req_body.Zip
-	products := req_body.Products
+	err := json.Unmarshal(body, &user_input)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	fmt.Println(user_input.Zip)
 
 	// refresh token
 	url := "https://api.kroger.com/v1/connect/oauth2/token"
@@ -100,55 +106,34 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 
 
   defer res.Body.Close()
-  body, _ := ioutil.ReadAll(res.Body)
+  body, _ = ioutil.ReadAll(res.Body)
+
+  // fmt.Println(res)
+  // fmt.Println(string(body))
+
 	var t Token
-	err := json.Unmarshal(body, &t)
+	err = json.Unmarshal(body, &t)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("refreshed access token")
 
+	// // find nearby stores
+	LocationHandler(user_input.Zip, t.Access_token)
 
-	// find nearby stores
-	url = fmt.Sprintf("https://api.kroger.com/v1/locations?filter.zipCode.near=%s&filter.limit=3", zip)
-
-	req, _ = http.NewRequest("GET", url, nil)
-
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJlZWNzNDk3cHQyLWVmZDQ0YzllNTJkZjcwYTA2YjcwYTYxOTE0MmE0YjU2ODgyNDgzOTM0Mzk0ODc0MjgzMyIsImV4cCI6MTY0ODQ4MDYyOCwiaWF0IjoxNjQ4NDc4ODIzLCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjU0Y2I0MTc5LWExYTQtNWE2MC05ODUxLTI2YTRiNzczZTVlZSIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjQ4NDc4ODI4MDYzNjM4NTc3LCJhenAiOiJlZWNzNDk3cHQyLWVmZDQ0YzllNTJkZjcwYTA2YjcwYTYxOTE0MmE0YjU2ODgyNDgzOTM0Mzk0ODc0MjgzMyJ9.aAPRlMhWsvS7uc-_Ez56D9RheeobXCwqJKoR6olxy5GGmEInL8IvJcB15AZ2sHELzdPf51DAzXgW9CTuoSsTh0IJ7OtuCRWgkLpzHwpbhkRjlLT0V9hvszj4NhgwyA5y7J6at7FxLGfuZ1E_1c024vcVROB2pRzYXeeAdsNfLSQbqKnXd6uwToQ5qk_JSmJdLiTxCgGxY-5YIT7z-1VCiARqyjm7aY6OhT8jEbA5YjHCOLbepybH6GOxvUZlgn3uN8Uo8Yp-Vd_I2GKkgkunK0JxML7TEVDoXVFtNHaprJ9uARLUAF2B7LgnhrVRX3Y9d1dAoC8pi3L8JWggRSyOIg")
-
-	res, _ = http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ = ioutil.ReadAll(res.Body)
-
-	fmt.Println(res)
-	fmt.Println(string(body))
-	// // store_ids = []
-	//
-	// search for products
+	// // search for products
 	locationId := "01400943"
-	url = fmt.Sprintf("https://api.kroger.com/v1/products?filter.term=%s&filter.locationId=%s", products, locationId)
-
-  req, _ = http.NewRequest("GET", url, nil)
-
-  req.Header.Add("Accept", "application/json")
-  req.Header.Add("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJlZWNzNDk3cHQyLWVmZDQ0YzllNTJkZjcwYTA2YjcwYTYxOTE0MmE0YjU2ODgyNDgzOTM0Mzk0ODc0MjgzMyIsImV4cCI6MTY0ODQ4MDYyOCwiaWF0IjoxNjQ4NDc4ODIzLCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjU0Y2I0MTc5LWExYTQtNWE2MC05ODUxLTI2YTRiNzczZTVlZSIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjQ4NDc4ODI4MDYzNjM4NTc3LCJhenAiOiJlZWNzNDk3cHQyLWVmZDQ0YzllNTJkZjcwYTA2YjcwYTYxOTE0MmE0YjU2ODgyNDgzOTM0Mzk0ODc0MjgzMyJ9.aAPRlMhWsvS7uc-_Ez56D9RheeobXCwqJKoR6olxy5GGmEInL8IvJcB15AZ2sHELzdPf51DAzXgW9CTuoSsTh0IJ7OtuCRWgkLpzHwpbhkRjlLT0V9hvszj4NhgwyA5y7J6at7FxLGfuZ1E_1c024vcVROB2pRzYXeeAdsNfLSQbqKnXd6uwToQ5qk_JSmJdLiTxCgGxY-5YIT7z-1VCiARqyjm7aY6OhT8jEbA5YjHCOLbepybH6GOxvUZlgn3uN8Uo8Yp-Vd_I2GKkgkunK0JxML7TEVDoXVFtNHaprJ9uARLUAF2B7LgnhrVRX3Y9d1dAoC8pi3L8JWggRSyOIg")
-
-  res, _ = http.DefaultClient.Do(req)
-
-  defer res.Body.Close()
-  body, _ = ioutil.ReadAll(res.Body)
-
-  fmt.Println(res)
-  fmt.Println(string(body))
+	ProductHandler(user_input.Products, locationId, t.Access_token)
 }
 
 func main() {
 	fmt.Printf("Starting server at port 8080\n")
-	http.HandleFunc("/hello", basicGetHandler)
+	// http.HandleFunc("/hello", basicGetHandler)
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	log.Fatal(err)
+	// }
+	http.HandleFunc("/list", listHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-	// LocationHandler("48104")
-	// ProductHandler("tempeh", "01400943")
 }
